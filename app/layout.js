8e752f77e9cdb93249ad50f5c3fc680aeb00c31b
Tanjimul Islam Sabbir <tanjimulislamsabbir02@gmail.com>
2024-05-01 00:50:39 +0600

    working on login

--- app/layout.js
+++ app/layout.js
@@ -1,8 +1,16 @@
-import { Inter } from "next/font/google";
 import "./globals.css";
 import { AntdRegistry } from "@ant-design/nextjs-registry";
+import { Inter, Roboto_Mono } from 'next/font/google'
 
-const inter = Inter({ subsets: ["latin"] });
+export const inter = Inter({
+  subsets: ['latin'],
+  display: 'swap',
+})
+
+export const roboto_mono = Roboto_Mono({
+  subsets: ['latin'],
+  display: 'swap',
+})
 
 export const metadata = {
   title: "Project Management",
@@ -12,7 +20,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>
+      <body className={roboto_mono.className}>
         <AntdRegistry>{children}</AntdRegistry>
       </body>
     </html>
--- app/login.js
+++ app/login.js
@@ -1,100 +1,77 @@
 "use client"
-import { MdOutlineAlternateEmail } from "react-icons/md";
-import { BiLockOpen } from "react-icons/bi";
-import { BiSolidShow } from "react-icons/bi";
-import { BsEyeSlash } from "react-icons/bs";
-import { useState } from "react";
-import { debounce } from "./utils/Debounce";
-import Link from "next/link";
-import IsValidEmail from "./utils/IsEmailValid";
-import "./style/login.css"
+import React, { useState } from 'react';
+import { Button, Checkbox, Form, Input } from 'antd';
+import toast from 'react-hot-toast';
 
+const LoginForm = () => {
+  const [erro, setError] = useState(false)
+  const onFinish = (values) => {
+    if (values.password) setError(false);
 
-
-function LoginForm() {
-  const [formData, setFormData] = useState({ email: "", password: "" });
-  const [loginError, setLoginError] = useState("");
-  const [open, setOpen] = useState(false);
-  const [valid, setValid] = useState(true);
-  // const [login, { data: loginUser, isError, isLoading, error }] = useLoginMutation();
-  // const userData = useSelector(state => state.auth) || {};
-  // const navigate = useNavigate();
-
-  const handleFormData = (event) => {
-    setFormData(prev => ({ ...prev, [event.target.name]: event.target.value }));
-
-    if (event.target.name === "email") {
-      emailValidityChecking(event.target.value)
+    if (values.email && values.password) {
+      toast.success("Login successful!")
     }
-  }
+  };
 
-  const handleEmail = (email) => {
-    const emailValidity = IsValidEmail(email);
-    setValid(emailValidity);
-  }
-  const emailValidityChecking = debounce(handleEmail, 700);
-
-
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    if (formData?.email && formData?.password) {
-      login({ data: { ...formData } })
-    }
-  }
-
+  const onFinishFailed = (errorInfo) => {
+    setError(true)
+  };
 
   return (
-    <form onSubmit={handleSubmit} className="h-screen w-screen flex items-center justify-center">
-      <div className="flex flex-col items-center space-y-12">
-        <div className="relative">
-          {
-            formData?.email !== "" ||
-            <div className="absolute top-5 left-4 flex items-center space-x-1 text-sm opacity-60">
-              <MdOutlineAlternateEmail className="placeHolderIcon" />
-              <span className="label">Your Email</span>
-            </div>
-          }
+    <div>
+      <div className='h-screen w-full flex items-center justify-center border border-gray-600 shadow-2xl rounded-lg'>
 
-          <input type="text"
-            className={`inputBox ${valid || "invalidEmailInput"} min-w-[260px] sm:min-w-[400px] md:min-w-[540px]`}
+        <Form
+          className='w-[250px] sm:w-[300px] md:w-[400px] '
+          name="basic"
+          layout="vertical"
+          initialValues={{
+            remember: true,
+          }}
+          onFinish={onFinish}
+          onFinishFailed={onFinishFailed}
+          autoComplete="off"
+        >
+          <h1 className='text-3xl mb-10 text-center'>Login</h1>
+          <Form.Item
+            label="Email"
             name="email"
-            onChange={(event) => handleFormData(event)}
-            required
-          />
-          {valid || <small className="invalidEmailError block text-start">Please enter a valid email address.</small>}
-        </div>
+            rules={[
+              {
+                type: 'email',
+                message: 'Please enter a valid email!',
+              },
+              {
+                required: true,
+                message: 'Please input your email!',
+              },
+            ]}
+          >
+            <Input />
+          </Form.Item>
 
-        {/* Password */}
-        <div className="relative">
-          {formData?.password !== "" ||
-            <div className="absolute top-4 left-4 flex items-center space-x-1 text-sm opacity-60">
-              <BiLockOpen className="placeHolderIcon" />
-              <span className="label">Password</span>
-            </div>
-          }
-
-          <input type={open ? "text" : "password"}
-            className="inputBox min-w-[260px] sm:min-w-[400px] md:min-w-[540px]"
+          <Form.Item
+            label="Password"
             name="password"
-            onChange={(event) => handleFormData(event)}
-            required
-          />
+            rules={[
+              {
+                required: true,
+                message: 'Please input your password!',
+              },
+            ]}
+          >
+            <Input.Password />
+          </Form.Item>
 
-          {/* password be showed or not be showed */}
-          <p className="absolute top-4 right-5 cursor-pointer">
-            {open ? <BiSolidShow className="eye" onClick={() => setOpen(!open)} /> : <BsEyeSlash className="eye" onClick={() => setOpen(!open)} />}
-          </p>
-
-          {/* Form button */}
-          <div>
-            <button type="submit" className="signInBtn w-[260px] sm:w-[400px] md:w-[540px]">Login </button>
-          </div>
-
-        </div>
+          <Form.Item>
+            <Button type="primary" className={erro && "mt-5"} htmlType="submit">
+              Submit
+            </Button>
+          </Form.Item>
+        </Form>
       </div>
-      {/* {loginError !== "" && <Error message={loginError} />} */}
-    </form>
+    </div>
   );
-}
+};
 
 export default LoginForm;
